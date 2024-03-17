1bbb89d6f5571dae6a59057839b11d1417cd061f
서형석 <ming0223@naver.com>
2024-03-17 17:07:10 +0900

    Scene 추가 + 싱글톤
    기본 씬들 만들고, Manager용으로 싱글톤 구현.

---
+++ Assets/Singleton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                Instance = (T)FindObjectOfType(typeof(T));
+            }
+
+            if(instance == null)
+            {
+                GameObject obj = new GameObject(typeof(T).Name, typeof(T));
+                instance = obj.GetComponent<T>();
+            }
+        }
+    }
+}
