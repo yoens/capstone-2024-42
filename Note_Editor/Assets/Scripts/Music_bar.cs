cb66c6d63f5d54564a5f588f62ff9bed51c93ca3
yoens <yoensu0228@naver.com>
2024-05-12 07:50:15 +0900

    노트 에디터 드래그 수정기능 추가

--- Note_Editor/Assets/Scripts/Music_bar.cs
+++ Note_Editor/Assets/Scripts/Music_bar.cs
@@ -1,46 +1,34 @@
 using UnityEngine;
-using UnityEngine.UI; 
-using UnityEngine.Networking;
-using System.Collections;
+using UnityEngine.UI;
+using TMPro;  // TextMeshPro 네임스페이스 추가
+
 public class Music_bar : MonoBehaviour
 {
-    public Slider progressSlider; // 진행 상태를 나타내는 슬라이더
-    private AudioSource audioSource;
+    public Slider progressSlider;
+    public AudioSource audioSource;
+    public TMP_Text timeText;  // TextMeshPro 텍스트 객체 참조
 
-    void Awake()
-    {
+    void Start() {
+        progressSlider.onValueChanged.AddListener(delegate { UpdateTimeDisplay(); });
         audioSource = GetComponent<AudioSource>();
     }
 
-    void Start()
-    {
-        if (progressSlider != null)
-        {
-            progressSlider.onValueChanged.AddListener(SetMusicPosition);
-        }
-    }
-
-    void Update()
-    {
-        if (audioSource.isPlaying)
-        {
-            UpdateSliderPosition();
-        }
-    }
-
-    void UpdateSliderPosition()
-    {
-        if (audioSource.clip != null)
-        {
+    void Update() {
+        if (audioSource.isPlaying) {
             progressSlider.value = audioSource.time / audioSource.clip.length;
+            UpdateTimeDisplay();
         }
     }
 
-    public void SetMusicPosition(float value)
-    {
-        if (audioSource.clip != null)
-        {
-            audioSource.time = value * audioSource.clip.length;
-        }
+    void UpdateTimeDisplay() {
+        float time = progressSlider.value * audioSource.clip.length;
+        int minutes = (int)time / 60;
+        int seconds = (int)time % 60;
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);  // 텍스트 업데이트
+    }
+
+    public void SetMusicPosition(float value) {
+        audioSource.time = value * audioSource.clip.length;
+        UpdateTimeDisplay();  // 위치 조정 시 텍스트도 업데이트
     }
 }
